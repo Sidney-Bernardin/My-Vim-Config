ce311ce5f0672a07daf11e3b557dc2ab772655c7
Sidney-Bernardin <40411946+Sidney-Bernardin@users.noreply.github.com>
2020-04-28 09:21:55 -0400

    Create do_plugins.sh

---
+++ do_plugins.sh
@@ -0,0 +1,10 @@
+cd
+cd -p .vim/pack/plugins/start
+
+sudo git clone https://github.com/preservim/nerdtree.git &&
+sudo git clone https://github.com/drewtempelmeyer/palenight.vims &&
+sudo git clone https://github.com/ervandew/supertab &&
+sudo git clone https://github.com/calviken/vim-gdscript3 &&
+sudo git clone https://github.com/kiteco/vim-plugin &&
+
+echo Done!
